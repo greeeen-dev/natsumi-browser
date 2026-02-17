7477ae37e6f2d61a9a2b091b0e45f8c24aab2c74
green. <41323182+greeeen-dev@users.noreply.github.com>
2026-02-17 10:55:46 +0000

    Fix Windows installer

--- installer/windows-installer.bat
+++ installer/windows-installer.bat
@@ -267,7 +267,7 @@ echo.
 
 :: config.js
 echo    [COPY] Attempting to copy config.js...
-copy /y "%TEMP_DIR%\fx-autoconfig\program\config.js" "%PROFILE_ROOT_PATH%\"
+copy /y "%TEMP_DIR%\fx-autoconfig\program\config.js" "%FINAL_INSTALL_PATH%\"
 if %errorlevel% neq 0 (
     echo.
     echo    [ERROR] Failed to copy config.js!
@@ -280,6 +280,7 @@ if %errorlevel% neq 0 (
 )
 :: Rename config.js for LibreWolf
 if "%SELECTION%"=="4" (
+    copy /y "%TEMP_DIR%\fx-autoconfig\program\config.js" "%PROFILE_ROOT_PATH%\"
     rename-item  "%PROFILE_ROOT_PATH%\config.js" -newName "librewolf.overrides.cfg"
 )
 
