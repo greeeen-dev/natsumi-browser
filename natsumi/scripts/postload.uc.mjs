6e30938fc9860043af69cc6ff05dda3cc33ae5d2
green. <41323182+greeeen-dev@users.noreply.github.com>
2026-06-15 21:23:36 +0100

    Hide top and bottom toolbars on onboarding

--- natsumi/scripts/postload.uc.mjs
+++ natsumi/scripts/postload.uc.mjs
@@ -78,7 +78,7 @@ class NatsumiPostloadManager {
             #PersonalToolbar, #nav-bar-customization-target, #PanelUI-button, #tabbrowser-tabpanels,
             #nora-statusbar, #status-bar, #urlbar, #panel-sidebar-select-box, #notifications-toolbar,
             #sidebar-main, #TabsToolbar-customization-target, #nav-bar-overflow-button, #tabbrowser-tabbox,
-            #natsumi-pinned-toolbar, #nav-bar {
+            #natsumi-pinned-toolbar, #natsumi-top-toolbar, #natsumi-bottom-toolbar, #nav-bar {
                 opacity: 0;
                 pointer-events: none !important;
             }
